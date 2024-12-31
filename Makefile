62780d7fab3589433bc1c250fff4c2dafa63b527
p12s <working-tam@yandex.com>
2024-12-31 21:42:29 +0300

    Добавлен простой c++ код

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+
+build:
+	clang++ hello.cpp -o hello
+
+run:
+	clang++ hello.cpp && ./a.out
+
---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::cout << "🔥🦆" << std::endl;
+
+  int value = 42;
+  std::string title = "Some string";
+  std::cout << title << std::endl;
+
+  std::cin >> title;
+  std::cout << title << std::endl;
+}
