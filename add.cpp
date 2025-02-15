31610c11e941fa3fa2a5be5116a30029e470d460
Pavel V <working-tam@yandex.com>
2025-02-15 14:44:12 +0300

    Formatted github action workflow

--- add.cpp
+++ add.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+int add(const int a, const int b) {
+    return a + b;
+}
+
 int main() {
     int a, b;
+    std::cout << "Введите первое число: ";
     std::cin >> a;
+    std::cout << "Введите второе число: ";
     std::cin >> b;
-
-    std::cout << a + b << std::endl;
+    
+    std::cout << "Сумма: " << add(a, b) << std::endl;
+    return 0;
 }
+
+
+
+
